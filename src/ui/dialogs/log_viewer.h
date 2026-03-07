4af71c1c60e245be36906e130bcfbc6ff4e7d41e
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2026-03-07 20:20:38 +0100

    Added ./src/ui/dialogs/log_viewer.h

---
+++ src/ui/dialogs/log_viewer.h
@@ -0,0 +1,53 @@
+// 5250ng - A modern IBM TN5250 terminal emulator                                                                                                                                                            
+// Copyright (C) 2025-2026 Remi GASCOU (Podalirius)                                                                                                                                                          
+//                                                                                                                                                                                                           
+// This program is free software: you can redistribute it and/or modify                                                                                                                                      
+// it under the terms of the GNU General Public License as published by                                                                                                                                      
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.                                                                                                                                                                       
+//                                                                                                                                                                                                           
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include "logger/logger.h"
+#include "session/worker.h"
+#include "ui/widgets/Frameless/BaseFramelessWindow.h"
+#include <QHBoxLayout>
+#include <QLineEdit>
+#include <QPlainTextEdit>
+#include <QPushButton>
+#include <QShortcut>
+#include <QVBoxLayout>
+
+class LogViewerDialog : public ui::widgets::BaseFramelessWindow {
+    Q_OBJECT
+  public:
+    explicit LogViewerDialog(QWidget *parent = nullptr);
+    explicit LogViewerDialog(tn5250::session::Worker *worker, QWidget *parent = nullptr);
+    ~LogViewerDialog() override = default;
+
+  private slots:
+    void onLogMessage(logger::LogLevel level, const QString &message);
+    void onSessionLogAppended(const QString &line);
+    void onFindToggle();
+    void onFindReturnPressed();
+    void onFindNext();
+    void onFindPrev();
+
+  private:
+    QWidget *m_findBar;
+    QLineEdit *m_findEdit;
+    QPushButton *m_nextBtn;
+    QPushButton *m_prevBtn;
+    QShortcut *m_shortcutFind;
+    QPlainTextEdit *m_text;
+    void loadExisting();
+    tn5250::session::Worker *m_worker = nullptr;
+};
