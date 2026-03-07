417fb967f77ab5b2c0706e85ba746a319fe67dc9
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2026-03-07 20:18:12 +0100

    Added ./src/agent/provider.h

---
+++ src/agent/provider.h
@@ -0,0 +1,50 @@
+// 5250ng - A modern IBM TN5250 terminal emulator
+// Copyright (C) 2025-2026 Remi GASCOU (Podalirius)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include <QObject>
+#include <QString>
+#include <QStringList>
+
+namespace agent {
+
+class Provider : public QObject {
+    Q_OBJECT
+
+  public:
+    explicit Provider(QObject *parent = nullptr) : QObject(parent) {}
+    virtual ~Provider() = default;
+
+    virtual QString id() const = 0;
+    virtual QString displayName() const = 0;
+    virtual QStringList availableModels() const = 0;
+    virtual void sendMessage(const QString &userMessage, const QString &systemContext) = 0;
+    virtual void cancel() = 0;
+    virtual bool isConfigured() const = 0;
+
+    virtual void setApiKey(const QString &key) = 0;
+    virtual void setModel(const QString &model) = 0;
+    virtual QString apiKey() const = 0;
+    virtual QString model() const = 0;
+
+  signals:
+    void responseReceived(const QString &text);
+    void responseError(const QString &error);
+    void streamingChunk(const QString &chunk);
+};
+
+} // namespace agent
