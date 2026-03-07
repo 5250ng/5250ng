f956826a7df40c9bd6507fdb9d110c7634102299
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2026-03-07 20:18:36 +0100

    Added ./src/main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,129 @@
+// 5250ng - A modern IBM TN5250 terminal emulator                                                                                                                                                            
+// Copyright (C) 2025-2026 Remi GASCOU (Podalirius)                                                                                                                                                          
+//                                                                                                                                                                                                           
+// This program is free software: you can redistribute it and/or modify                                                                                                                                      
+// it under the terms of the GNU General Public License as published by                                                                                                                                      
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.                                                                                                                                                                       
+//                                                                                                                                                                                                           
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include "logger/logger.h"
+#include "session/config.h"
+#include "session/manager.h"
+#include "ui/main_window.h"
+#include "ui/themes/manager.h"
+#include "ui/themes/font_loader.h"
+#include "ui/themes/terminal_theme_manager.h"
+#include <QApplication>
+#include <QCommandLineOption>
+#include <QCommandLineParser>
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    app.setApplicationName("5250ng");
+    app.setApplicationVersion("0.5.0");
+    app.setOrganizationName("5250ng");
+
+    // Parse command line arguments
+    QCommandLineParser parser;
+    parser.setApplicationDescription("5250ng - TN5250 Terminal Emulator");
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    // Debug option
+    QCommandLineOption debugOption(QStringList() << "d" << "debug", "Enable debug output");
+    parser.addOption(debugOption);
+
+    // Host option
+    QCommandLineOption hostOption(QStringList() << "H" << "host", "Hostname or IP address to connect to", "hostname");
+    parser.addOption(hostOption);
+
+    // Port option
+    QCommandLineOption portOption(QStringList() << "p" << "port", "Port number (default: 23)", "port", "23");
+    parser.addOption(portOption);
+
+    // TLS option
+    QCommandLineOption tlsOption(QStringList() << "tls", "Use TLS/SSL encryption");
+    parser.addOption(tlsOption);
+
+    parser.process(app);
+
+    // Set debug level if requested
+    if (parser.isSet(debugOption)) {
+        logger::Logger::instance()->setLogLevel(logger::LogLevel::Debug);
+        logger::Logger::instance()->debug("Debug mode enabled");
+    }
+
+    // Create session config from command line if host is provided
+    session::SessionConfig autoConnectConfig;
+    bool autoConnect = false;
+
+    if (parser.isSet(hostOption)) {
+        QString hostname = parser.value(hostOption);
+        bool portOk = false;
+        int portInt = parser.value(portOption).toInt(&portOk);
+
+        if (!portOk || portInt < 1 || portInt > 65535) {
+            logger::Logger::instance()->error(
+                QString("Invalid port number: %1").arg(parser.value(portOption))
+            );
+            return 1;
+        }
+
+        quint16 port = static_cast<quint16>(portInt);
+        bool useTLS = parser.isSet(tlsOption);
+
+        autoConnectConfig.setName("Command Line Session");
+        autoConnectConfig.setHostname(hostname);
+        autoConnectConfig.setPort(port);
+        autoConnectConfig.setUseTLS(useTLS);
+        autoConnectConfig.setDeviceName("IBM-3179-2");
+        autoConnectConfig.setScreenRows(24);
+        autoConnectConfig.setScreenCols(80);
+
+        if (autoConnectConfig.isValid()) {
+            autoConnect = true;
+            logger::Logger::instance()->debug(
+                QString("Auto-connect configured: %1:%2%3")
+                    .arg(hostname)
+                    .arg(port)
+                    .arg(useTLS ? " (TLS)" : "")
+            );
+        } else {
+            logger::Logger::instance()->error(
+                "Invalid connection parameters from command line"
+            );
+            return 1;
+        }
+    }
+
+    // Register bundled terminal fonts before any widget creation
+    ui::themes::loadBundledFonts();
+
+    // Load themes early so UI can query theme colors
+    ui::themes::ThemeManager::instance().loadBuiltinThemes();
+    // Optionally set a default theme
+    ui::themes::ThemeManager::instance().setCurrentTheme("dark");
+
+    // Load terminal themes (builtin + user-created)
+    ui::themes::TerminalThemeManager::instance().loadBuiltinThemes();
+    ui::themes::TerminalThemeManager::instance().loadUserThemes();
+
+    MainWindow win;
+    win.show();
+
+    // Auto-connect if host was provided
+    if (autoConnect) {
+        win.autoConnect(autoConnectConfig);
+    }
+
+    return app.exec();
+}
