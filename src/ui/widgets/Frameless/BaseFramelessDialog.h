67d41009087b03d65856ee358d3943869ffe78d9
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2026-03-07 20:21:31 +0100

    Added ./src/ui/widgets/Frameless/BaseFramelessDialog.h

---
+++ src/ui/widgets/Frameless/BaseFramelessDialog.h
@@ -0,0 +1,49 @@
+// 5250ng - A modern IBM TN5250 terminal emulator                                                                                                                                                            
+// Copyright (C) 2025-2026 Remi GASCOU (Podalirius)                                                                                                                                                          
+//                                                                                                                                                                                                           
+// This program is free software: you can redistribute it and/or modify                                                                                                                                      
+// it under the terms of the GNU General Public License as published by                                                                                                                                      
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.                                                                                                                                                                       
+//                                                                                                                                                                                                           
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include "ui/widgets/Frameless/TitleBar.h"
+#include <QDialog>
+#include <QVBoxLayout>
+
+namespace ui::widgets {
+
+class BaseFramelessDialog : public QDialog {
+    Q_OBJECT
+  public:
+    explicit BaseFramelessDialog(QWidget *parent = nullptr);
+    ~BaseFramelessDialog() override = default;
+
+    void setWindowTitle(const QString &title);
+    TitleBar *titleBar() const { return m_titleBar; }
+    QVBoxLayout *contentLayout() const { return m_contentLayout; }
+
+  private:
+    TitleBar *m_titleBar;
+    QVBoxLayout *m_contentLayout;
+
+    // Drag
+    bool m_dragging = false;
+    QPoint m_dragOffset;
+
+  private slots:
+    void onTitleMousePressed(const QPoint &globalPos);
+    void onTitleMouseMoved(const QPoint &globalPos);
+    void onTitleMouseReleased();
+};
+
+} // namespace ui::widgets
